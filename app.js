5dad4fc7f4f62406d033bf5a7c6be6fa44e26c02
Jst77 <justinte7@gmail.com>
2022-03-06 14:16:42 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// linkedin logo button 
+function linkedinlogo() { 
+    window.open('https://www.linkedin.com/in/justinte/', '_blank').focus();
+}
+
+linkedinlogo();
+
+// gmail logo button
+
+function gmaillogo() { 
+    window.open('mailto:justinte7@gmail.com', '_blank').focus();
+}
+
+gmaillogo();
